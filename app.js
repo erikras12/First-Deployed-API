de89b3db2ee07ad2f3d2453c076e33d8afe9ce01
erikras12 <erikras12@hotmail.com>
2025-01-21 10:20:04 +0000

    m

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const logger = require('./logger')
+const cors = require('cors')
 const app = express()
 
 
-
-
+// Middleware
+app.use(cors())
 app.use(express.json())
 app.use(logger) //without first route argument (func runs everywhere)
 
